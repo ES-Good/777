a27149337bd0c655a5be9d00d009d13aff0b1e5f
Максим Казачков <evgenshtanko@ya.ru>
2020-11-07 02:28:32 +0400

    't'

---
+++ js/common.js
@@ -0,0 +1,7 @@
+let btn = document.querySelector('.rotate-wheel-btn');
+
+document.onclick = function (event) {
+    if(event.target.classList.contains('rotate-wheel-btn')){
+        console.log('+')
+    }   
+}
