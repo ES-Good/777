2a21d46834e83212e351a040ed2098e6526b3677
Максим Казачков <evgenshtanko@ya.ru>
2020-11-07 04:57:39 +0400

    'end'

--- js/common.js
+++ js/common.js
@@ -1,7 +1,8 @@
 let btn = document.querySelector('.rotate-wheel-btn');
+let wheel = document.querySelector('.wheel')
 
 document.onclick = function (event) {
     if(event.target.classList.contains('rotate-wheel-btn')){
-        console.log('+')
+        wheel.style.transform = 'rotate(1140deg)' //шаг 60deg
     }   
 }
