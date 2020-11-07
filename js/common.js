ebdfc82801cc1e9996c471847c6e578a9d7b31a0
ES-Good <46601455+ES-Good@users.noreply.github.com>
2020-11-08 03:38:21 +0400

    'modal'

--- js/common.js
+++ js/common.js
@@ -6,3 +6,11 @@ document.onclick = function (event) {
         wheel.style.transform = 'rotate(1140deg)' //шаг 60deg
     }   
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var options = {
+        opacity: 0.7,
+    }
+    var instances = M.Modal.init(elems, options);
+  });
