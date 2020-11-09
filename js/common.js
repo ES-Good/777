afe443900bd33d57c57351368238e7d27c0c5480
Максим Казачков <evgenshtanko@ya.ru>
2020-11-09 15:53:24 +0400

    'languages'

--- js/common.js
+++ js/common.js
@@ -14,3 +14,11 @@ document.addEventListener('DOMContentLoaded', function() {
     }
     var instances = M.Modal.init(elems, options);
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var options = {
+        constrainWidth: false
+    }
+    var instances = M.Dropdown.init(elems, options);
+  });
